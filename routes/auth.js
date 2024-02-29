f63f2fa99de4f8fc077db8e0db8365032fd3b7f1
Gannub <gannubvv@gmail.com>
2024-02-29 18:34:59 +0700

    add jwt token response -> register, login, logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
-const {register} = require('../controllers/auth');
+const {register, login, logout, getMe} = require('../controllers/auth');
+
+const {protect} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/me', protect ,getMe);
+router.get('/logout', logout);
+
+
 
 module.exports = router;
